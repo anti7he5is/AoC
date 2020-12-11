761f82e539b635d45feb560202ee97c6ae9ee0fa
anti7he5is <maximilien.stevens@gmail.com>
2020-12-11 20:56:04 +1100

    Day 10 - Readability changes.

--- 2020/Day10/advent10-1.py
+++ 2020/Day10/advent10-1.py
@@ -9,24 +9,28 @@ if len(sys.argv) != 2:
 # Provide intial joltage
 input = [0]
 
+# Add input joltages
 with open(sys.argv[1]) as f:
     for line in f.readlines():
         input.append(int(line.strip("\n")))
 
-# Provide final joltage (max + 3)
+# Provide final joltage (max(input) + 3)
 input.append(max(input)+3)
 
+# Order the joltages
 input.sort()
 
+# Initialise counters for 1, 2 and 3 jolt steps in input
 joltDiffs = [0, 0, 0]
 
+# Calculate the input joltage difference between consecutive values
 for i in range(0, len(input)-1):
     diff = input[i+1] - input[i]
     if diff > 3 or diff == 0:
         print("Malformed input. Diff 0 or > 3.")
     else:
+        # Increment the counter
         joltDiffs[diff-1] += 1
 
-print(joltDiffs)
-print(joltDiffs[0] * joltDiffs[2])       
+print("Part 1: %i" % (joltDiffs[0] * joltDiffs[2]))      
         
--- 2020/Day10/advent10-2.py
+++ 2020/Day10/advent10-2.py
@@ -17,51 +17,58 @@ with open(sys.argv[1]) as f:
 input.append(max(input)+3)
 
 input.sort()
-print(input)
 
-joltDiffs = [0, 0, 0]
-
-longestRun = 0
+# Initialise counters for runs of 1 jolt differences
+# Observe in input data there are NO 2 jolt runs, reducing possible paths
 thisRun = 0
-lastDiff = 0
 runs = []
 
+# Initialise lastDiff to check when a run ends
+lastDiff = 0
+
+# For joltages in the input, record the length of runs of 1 jolt differences
 for i in range(0, len(input)-1):
     diff = input[i+1] - input[i]
     if diff > 3 or diff == 0:
         print("Malformed input. Diff 0 or > 3.")
     else:
-        joltDiffs[diff-1] += 1
-        
+        # Check if looking at a new joltage difference (i.e. end of a run)
         if diff != lastDiff:
-            if diff == 3:
-                print((input[i+1], input[i], thisRun))
-                longestRun = max(thisRun, longestRun)
+            # If the last joltage difference was 1, store the run length
+            if lastDiff == 1:
                 runs.append(thisRun)
             thisRun = 1
         else:
+            # if within a run, increment the value
             thisRun += 1
+
     lastDiff = diff
 
-print(joltDiffs)
-print(joltDiffs[0] * joltDiffs[2])       
-
-print("Part 2")
+print("Part 2.")
 
 # find longest run of 1s.
-print("Longest run: %i" % longestRun)
+print("Longest run of 1 Jolt differences: %i" % max(runs))
 
-# Runs of 2 = *2, 3 = *4, 4= *7
+# As the max difference between joltage steps is 3, only 1 jolt runs will branch
+# Runs of 2 = *2, 3 = *4, 4= *7 --- 4x 1 jolts steps has 7 possible combinations
+# which retain the max 3 jolt difference between any two steps.
+
+# Initialise the possible path counter
 paths = 1
+
+# for each run of 1 jolt differences
+# multiply the possible path by the branch factor.
 for run in runs:
-    if run == 2:
+    if run == 1:
+        continue
+    elif run == 2:
         paths = paths * 2
     elif run == 3:
         paths = paths * 4
     elif run == 4:
         paths = paths * 7
     else:
-        print("Invalid run len: %i" %i)
+        print("Invalid run len: %i" % run)
 
-print(paths)
+print("Total possible paths: %i" % paths)
 
