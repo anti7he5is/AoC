86460ec8ca816c5fee438fa61133c0eb4f01bc7b
anti7he5is <maximilien.stevens@gmail.com>
2020-12-17 17:40:32 +1100

    Day 13 - Part 2 Initial Commit.

--- 2020/Day13/advent13-2.py
+++ 2020/Day13/advent13-2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys
+import sys, math
 
 if len(sys.argv) != 2:
     print("Please provide input.")
@@ -15,16 +15,59 @@ with open(sys.argv[1]) as f:
 waitTime = arrival
 busID = 0
 
+offsets = []
+offset = 0
+
+baseMod = 1
+
 for route in routes:
     if route == 'x':
+        offset += 1
         continue
+    
+    offsetMod = (int(route) - offset) % int(route)
+    offsets.append((int(route), offsetMod))
+    baseMod *= int(route)
+    offset += 1
 
-    wait = int(route) - ( arrival % int(route) )
-    if wait < waitTime:
-        busID = int(route)
-        waitTime = wait
+print(offsets)
+
+for i in range(0, len(offsets)-1):
+    for j in range(i+1, len(offsets)):
+        a, b = offsets[i][0], offsets[j][0]
+        if math.gcd(a,b) != 1:
+            print("GCD Error.")
+            print("gcd(%i,%i) = %i" %(a, b, math.gcd(a,b)))
+            sys.exit(1)
 
 
-print("Part 1: %i" % (waitTime*busID))
+# Fill the CRT array
+CRT = list()
 
-print("Wait time: %i, busID: %i." % (waitTime, busID))
+for i in range(0, len(offsets)):
+    base = 1
+    for j in range(0, len(offsets)):
+        if i != j:
+            base *= offsets[j][0]
+    CRT.append(base)
+
+for i in range(0, len(CRT)):
+    mod = CRT[i] % offsets[i][0]
+    if mod != offsets[i][1]:
+        test = 2
+        solved = False
+        while not solved:
+            if (mod*test) % offsets[i][0] != (offsets [i][1]):
+                test += 1
+            else:
+                solved = True
+
+        newMod = (CRT[i] * test) % offsets[i][0]
+        
+        CRT[i] = CRT[i] * test
+
+earliest = sum(CRT) % baseMod
+print("Earliest: %i mod(%i) = %i" % (sum(CRT), baseMod, earliest))
+
+for offset in offsets:
+    print("Route, Offset: %i, %i -> Offset from Earliest: %i" % (offset[0], offset[1], earliest % offset[0]))
